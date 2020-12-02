6e48e1b571dfb94bb402dcb18e6388d86211d1cb
Alexander Feder <alexanderfeder@macbook-pro.local>
2020-12-02 18:43:15 +0100

    solve second day part one

--- 2020/javascript/02/start.js
+++ 2020/javascript/02/start.js
@@ -1,3 +1,43 @@
-console.log("hello world");
+//---load puzzle
+fs = require('fs');
+var input = fs.readFileSync('inputAlFe.txt', 'utf8');
+var inputArray = input.split('\n');
+//---end load puzzle
 
-console.log("bla");
+var solveA = 0
+
+for (i in inputArray) {
+//	//store password
+	var activLine = inputArray[i];
+//	var activLine = inputArray[1];
+
+	//split the password and the rule 
+	var splitLine = activLine.split(':');
+	var rule = splitLine[0]; 
+	var password = splitLine[1]; 
+	
+	// split the rule in vars
+	var splitRule = rule.split(' ');
+	var policyLetter = splitRule[1];
+	var policyRule = splitRule[0];
+	
+	var SplitChar = policyRule.split("-");
+	var min = SplitChar[0];
+	var max = SplitChar[1];
+	
+	//check policy
+	var occurances = 0
+	for (var letter in password) {
+		if (password[letter] == policyLetter) {
+			occurances++;
+		}
+	}
+	if (occurances >= min) {
+		if (occurances <= max) {
+			solveA++;
+		}
+	}
+}
+
+
+console.log(solveA);
