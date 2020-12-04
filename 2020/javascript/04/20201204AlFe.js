c2418966a9b1f1a3c1b5d8a26de322ae1cb435ea
Alexander Feder <alexanderfeder@macbook-pro.local>
2020-12-05 00:09:47 +0100

    day 4 fertsch

--- 2020/javascript/04/20201204AlFe.js
+++ 2020/javascript/04/20201204AlFe.js
@@ -11,54 +11,58 @@ var PassportList = input.split('\n\n');
 
 for (i1 in PassportList) {
 	var activePassport = PassportList[i1];
-	activePassport = activePassport.replace(/ /g, '\n');
-	var valid0 = activePassport.match( /hcl|ecl|byr|iyr|eyr|hgt|pid/gi );
+	activePassport = activePassport.replace(/\n/g, ' ');
+	var valid0 = activePassport.match( /hcl|ecl|byr|iyr|eyr|hgt|pid/g );
 	if (valid0.length == 7) {
 		solveA++;
 		valid = true
 		
 		//if valid, then check for part 2
 		
-		activePassport = activePassport.split('\n')
+		activePassport = activePassport.split(' ')
 		for (i2 in activePassport) {
 			var checkLine = activePassport[i2];
 			var checkArray = checkLine.split(':');
 			var checkPosition = checkArray[0];
 			var checkValue = checkArray[1];
 			if (checkPosition == 'byr') {
-				if (checkValue >= 1920 && checkValue <= 2002) {
+				if (Number(checkValue) >= 1920 && Number(checkValue) <= 2002) {
 					valid = true;
 				} else {
 					valid = false;
 				}
 			} else if (checkPosition == 'iyr') {
-				if (checkValue >= 2010 && checkValue <= 2020) {
+				if (Number(checkValue) >= 2010 && Number(checkValue) <= 2020) { //20[1,2]\d
 					valid = true;
 				} else {
 					valid = false;
 				}
 			} else if (checkPosition == 'eyr') {
-				if (checkValue >= 2020 && checkValue <= 2030) {
+				if (Number(checkValue) >= 2020 && Number(checkValue) <= 2030) {
 					valid = true;
 				}else {
 					valid = false;
 				}
 			} else if (checkPosition == 'hgt') {
 				var unit = checkValue.match( /cm$|in$/ );
-				if (unit[0] == 'cm') {
-					value = checkValue.replace(/\D/g, '')
-					if (value >= 150 && value <=193) {
-						valid = true;
-					}else {
-						valid = false;
+				if (unit != null) {
+					if (unit[0] == 'cm') {
+						value = checkValue.replace(/\D/g, '') //159cm // Number(159cm) == 159
+						if (value >= 150 && value <=193) {
+							valid = true;
+						}else {
+							valid = false;
+						}
+					} else if (unit == 'in') {
+						value = checkValue.replace(/\D/g, '')
+						if (value >= 59 && value <=76) {
+							valid = true;
+						}else {
+							valid = false;
+						}
 					}
-				} else if (unit == 'in') {
-					value = checkValue.replace(/\D/g, '')
-					if (value >= 59 && value <=76) {
-						valid = true;
-					}else {
-						valid = false;
-					}
+				}else {
+					valid = false;
 				}
 			} else if (checkPosition == 'hcl') {
 				value = checkValue.match(/#[0-9a-f]{6}/g)
@@ -75,25 +79,25 @@ for (i1 in PassportList) {
 					valid = true;
 				}
 			} else if (checkPosition == 'pid') {
-				value = checkValue.match(/[0-9]{9}/g)
+				value = checkValue.match(/^[0-9]{9}$/g) //\d
 				if (value == null) {
 					valid = false;
 				} else {
 					valid = true;
 				}
-
 			}
-			console.log(valid)
-			console.log('###########################')
+			if (valid == false) {
+				break;
+			}
+		}
+		if (valid == true) {
+			console.log(activePassport);
+			solveB++;
 		}
 		
-		
 	}else {
-		valid = false
+		valid = false;
 	}
 }
-
-
-
 console.log(solveA);
 console.log(solveB);
