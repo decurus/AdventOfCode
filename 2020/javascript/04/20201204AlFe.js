4b47fab8985ecc1ef85bf252b7c07f4b3def66ce
Alexander Feder <alexanderfeder@macbook-pro.local>
2020-12-04 19:42:46 +0100

    day 4 part 1

--- 2020/javascript/04/20201204AlFe.js
+++ 2020/javascript/04/20201204AlFe.js
@@ -2,12 +2,21 @@
 fs = require('fs');
 var input = fs.readFileSync('inputAlFe.txt', 'utf8');
 //---end load puzzle
-var solveA;
-var solveB;
+var solveA = 0;
+var solveB = 0;
+
+var PassportList = input.split('\n\n');
+
+for (i1 in PassportList) {
+	var activePassport = PassportList[i1];
+	activePassport = activePassport.replace(/ /g, '\n');
+	var valid0 = activePassport.match( /hcl|ecl|byr|iyr|eyr|hgt|pid/gi )
+	if (valid0.length == 7) {
+		solveA++
+	}
+}
 
 
-
-console.log(input)
 
 console.log(solveA);
 console.log(solveB);
